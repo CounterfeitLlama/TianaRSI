607718323a5c2122053f618044157ef703c6a814
Andrew Turley <aturley123@gmail.com>
2016-10-02 22:27:39 -0400

    Began cross-platform and error handling

--- RSI/src/FaceDetector.java
+++ RSI/src/FaceDetector.java
@@ -8,17 +8,42 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 public class FaceDetector {
- 
+	final String OS = System.getProperty("sun.desktop");
+	final String HOME = System.getProperty("user.home");
+	
     public void run() {
- 
+    	//System.getProperties().list(System.out);
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("Starting...");
+        System.out.println("Operating System: " + OS);
 
-        // Open the facial recognition file
-        CascadeClassifier faceDetector = new CascadeClassifier("/Users/aturley/Downloads/opencv-master/data/haarcascades/haarcascade_frontalface_alt.xml");
-
+        // Open the facial recognition file depending on the OS
+        CascadeClassifier faceDetector = null;
+        if (OS.equals("windows")) {
+        	faceDetector = new CascadeClassifier(HOME + "\\Downloads\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_alt.xml");
+        }
+        else if (OS.equals("mac")) {
+        	faceDetector = new CascadeClassifier(HOME + "/Downloads/opencv-master/data/haarcascades/haarcascade_frontalface_alt.xml");
+        }
+        
+        // Check if faceDetector was initialized
+        if (faceDetector == null) {
+        	throw new NullPointerException("FaceDetector is null, bad OS");
+        }
+        
         // Read in face to detect
-        Mat image = Imgcodecs.imread("/Users/aturley/Desktop/startrek.jpg");
+        Mat image = null;
+        if (OS.equals("windows")) {
+        	image = Imgcodecs.imread(HOME + "\\Desktop\\startrek.jpg");
+        }
+        else if (OS.equals("mac")) {
+        	image = Imgcodecs.imread(HOME + "/Desktop/startrek.jpg");
+        }
+        
+        // Check if image was initialized properly
+        if (image == null) {
+        	throw new NullPointerException("Image is null, no image found");
+        }
  
         // Detect faces in the image
         MatOfRect faceDetections = new MatOfRect();
@@ -34,7 +59,13 @@ public class FaceDetector {
         }
  
         // Save the new file with rectangles displaying the detected faces
-        String filename = "/Users/aturley/Desktop/ouput.jpg";
+        String filename = null;
+        if (OS.equals("windows")) {
+        	filename = HOME + "\\Desktop\\output.jpg";
+        }
+        else if (OS.equals("mac")) {
+        	filename = HOME + "/Desktop/ouput.jpg";
+        }
         System.out.println(String.format("Done. Writing %s", filename));
         Imgcodecs.imwrite(filename, image);
     }
