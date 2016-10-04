9c4555808858c852d31f53986cc35b1772dd8a1a
Andrew Turley <aturley123@gmail.com>
2016-10-03 21:40:19 -0400

    Added escape key option to quit video capture

--- RSI/src/FaceDetector.java
+++ RSI/src/FaceDetector.java
@@ -1,15 +1,11 @@
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
 import javax.swing.*;
 import java.awt.image.*;
+import java.awt.event.*;
+import java.util.Scanner;
 
 public class FaceDetector {
 	final String OS = System.getProperty("os.name");
@@ -23,10 +19,16 @@ public class FaceDetector {
 	CascadeClassifier faceDetector;
 	CascadeClassifier eyeDetector;
 	
+	/**
+	 * Constructor for the FaceDetector class
+	 */
 	public FaceDetector() {
 		this.run();
 	}
 	
+	/**
+	 * Automatically run when constructor is created
+	 */
     public void run() {
     	// Load library
     	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -46,11 +48,28 @@ public class FaceDetector {
         JLabel vidpanel = new JLabel();
         jframe.setContentPane(vidpanel);
         jframe.setVisible(true);
+        jframe.addKeyListener(new KeyListener() {
+        	@Override
+        	public void keyTyped(KeyEvent e) { // May implement later for reasons
+        	}
+        	@Override
+        	public void keyReleased(KeyEvent e) { // May implement later for reasons
+        	}
+        	@Override
+        	public void keyPressed(KeyEvent e) { // This one is important
+        		String pressedKey = KeyEvent.getKeyText(e.getKeyCode());
+        		if (pressedKey.equals("Escape")) {
+        			jframe.dispose();
+        			quit();
+        		}
+        		//System.out.println("Key Character: " + e.getKeyChar() + "; Key Code: " + KeyEvent.getKeyText(e.getKeyCode()));
+        	}
+        	});
 
         while (true) {
             if (camera.read(frame)) {
             	detector.detect(faceDetector, frame);
-            	detector.detect(eyeDetector, frame);
+            	//detector.detect(eyeDetector, frame);
                 ImageIcon image = new ImageIcon(mat2Img(frame));
                 vidpanel.setIcon(image);
                 vidpanel.repaint();
@@ -59,10 +78,13 @@ public class FaceDetector {
     }
     
     public void initializePaths() {
-    	// Initialize all paths depending on OS
+    	/**
+    	 * Initialize all paths depending on OS
+    	 * Windows uses \ as delimeter and Mac uses \
+    	 */
         if (OS.equals("Windows 10")) {
         	faceDetectorPath = HOME + "\\Downloads\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_alt.xml";
-        	eyeDetectorPath = HOME + "\\Downloads\\opencv\\build\\haarcascades\\haarcascade_eye.xml";
+        	eyeDetectorPath = HOME + "\\Downloads\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml";
         	imagePath = HOME + "\\Desktop\\startrek.jpg";
         	outputPath = HOME + "\\Desktop\\output.jpg";
         }
@@ -74,6 +96,9 @@ public class FaceDetector {
         }
     }
     
+    /**
+     * Convert a Mat image to BufferedImage so it can be displayed
+     */
     public BufferedImage mat2Img(Mat m) {
     	int type = BufferedImage.TYPE_BYTE_GRAY;
         if (m.channels() > 1) {
@@ -83,4 +108,32 @@ public class FaceDetector {
         m.get(0, 0, ((DataBufferByte)image.getRaster().getDataBuffer()).getData()); // Get all the pixels
         return image;
      }
+    
+    /**
+     * Method to quit the application after escape key is pressed
+     */
+    public void quit() {
+		Scanner in = new Scanner(System.in);
+    	System.out.print("Quit program? (y/n): ");
+		String quit = in.next();
+		switch (quit) {
+			case "y":
+				System.out.println("Exiting...");
+				System.exit(0);
+			case "n":
+				System.out.println("Activate camera? (y/n)");
+				String activate = in.next();
+				switch (activate) {
+					case "y":
+						run();
+					case "n":
+						System.out.println("Exiting...");
+						System.exit(0);
+					default:
+						System.out.println("Invalid input.\n Exiting anyways...");
+						System.exit(1);
+				}
+		}
+		in.close();
+    }
 }
