f03902d9e35417c3d2d7ae3c37f62a71e1bb3253
Andrew Turley <aturley123@gmail.com>
2016-10-25 19:34:26 -0400

    Cleaned Main and started over

--- RSI/src/Main.java
+++ RSI/src/Main.java
@@ -1,32 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws InvalidInputException {
-		//System.getProperties().list(System.out);
-        System.out.println("Starting...");
-        System.out.println("Operating System: " + System.getProperty("os.name"));
-        System.out.println("OS Version: " + System.getProperty("os.version"));
-        System.out.println("Bit count: " + System.getProperty("sun.arch.data.model"));
-        System.out.println("Java Verion: " + System.getProperty("java.version"));
-        
-		Scanner input = new Scanner(System.in);
-		while (true) {
-			System.out.println("Activate which camera? (-1 for exit): ");
-			String num = input.next();
-			switch (num) {
-				case "-1":
-					System.out.println("Exiting...");
-					System.exit(0);
-				default:
-					try {
-						int cameraNumber = Integer.parseInt(num);
-						new FaceDetector(cameraNumber);
-						break;
-					}
-					catch (Exception e) {
-						throw new InvalidInputException();
-					}
-			}
-		}
+	public static void main(String[] args) {
+		System.out.println("Hello Tiana");
 	}
 }
